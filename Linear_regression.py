bcff4fa36b2fa2d388ee8f8f1ef86a6ac661e166
Pratik Bedre <pratikb.2805@gmail.com>
2019-12-15 15:50:43 +0530

    first try lin reg 15/12/19

--- Linear_regression.py
+++ Linear_regression.py
@@ -4,10 +4,13 @@ class Lin_reg:
     def __init__(self, data_in, data_out):                #data_in=training input,
         self.x = np.array(data_in)
         self.y = np.array(data_out)
-        self.row1 = np.size(self.x, axis=0)
-        self.col1 = np.size(self.x, axis=1)
+
+
+        self.row1= int(data_in.shape[0])
+        self.col1= int(data_in.shape[0])+1
         self.wt = np.random.randn(1, self.col1)
-        one= np.ones(self.row1, 1)
+        self.zero=np.zeros((1, self.col1),dtype=float)
+        one= np.ones((self.row1, 1),dtype=float)
         self.x = np.concatenate((one, self.x), axis=1)
         self.x = np.transpose(self.x)
         self.temp = self.wt
@@ -16,7 +19,7 @@ class Lin_reg:
         return np.matmul(self.wt, self.x)
 
     def y_cost(self):
-        return (0.5 / self.row1) * (np.sum((self.y - self.y_pred ** 2)))
+        return (0.5 / self.row1) * (np.sum(np.square(self.y - self.y_pred ))
 
     def update(self):
         for i in range(self.col1):
@@ -24,7 +27,7 @@ class Lin_reg:
         self.wt = self.wt - 0.01 * self.temp
 
     def train(self):
-        while self.temp!=0:
+        while self.temp.all()!= self.zero.all():
             self.y_pred
             self.y_cost()
             self.update()
@@ -32,8 +35,8 @@ class Lin_reg:
             pkl.dump(self.wt, weight)
         print(self.cost())
 
-x1=np.array([1,2,3,4,5])
-y1=np.array([2,3,4,5,6])
+x1=np.random.randn(5,1)
+y1=np.random.randn(1,5)
 model=Lin_reg(x1,y1)
 model.train()
 
